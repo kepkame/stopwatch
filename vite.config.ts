90e11eb9e169c1d0fa48843a37102c402e25ff58
kepkame <kepkame@gmail.com>
2025-08-20 00:11:00 +0300

    feat: add screenshots to manifest

--- vite.config.ts
+++ vite.config.ts
@@ -3,46 +3,53 @@ import react from '@vitejs/plugin-react';
 import { VitePWA, type ManifestOptions } from 'vite-plugin-pwa';
 
 const manifest: Partial<ManifestOptions> | false = {
-  theme_color: '#8936FF',
+  id: '/',
+  name: 'Stopwatch',
+  short_name: 'Stopwatch',
+  start_url: '.',
+  scope: '/',
+  display: 'standalone',
+  orientation: 'any',
   background_color: '#ffffff',
+  theme_color: '#e9773d',
+  dir: 'ltr',
+  lang: 'en',
   icons: [
+    { src: '/icon192.png', sizes: '192x192', type: 'image/png' },
     {
       purpose: 'maskable',
       sizes: '512x512',
-      src: 'icon512_maskable.png',
+      src: '/icon512_maskable.png',
       type: 'image/png',
     },
     {
       purpose: 'any',
       sizes: '512x512',
-      src: 'icon512_rounded.png',
+      src: '/icon512_rounded.png',
       type: 'image/png',
     },
   ],
   screenshots: [
     {
-      src: 'screenshots/desktop.png',
-      sizes: '1024x1024',
+      src: '/screenshots/desktop.png',
+      sizes: '1919x1079',
       type: 'image/png',
       form_factor: 'wide',
     },
     {
-      src: 'screenshots/mobile.png',
-      sizes: '1024x1024',
+      src: '/screenshots/mobile.png',
+      sizes: '375x667',
       type: 'image/png',
       form_factor: 'narrow',
     },
+    {
+      src: '/screenshots/mobile-small.png',
+      sizes: '360x800',
+      type: 'image/png',
+    },
   ],
-  orientation: 'any',
-  display: 'standalone',
-  dir: 'ltr',
-  lang: 'en',
-  name: 'SwopWatch',
-  start_url: '/',
-  short_name: 'SwopWatch',
 };
 
-// https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
@@ -64,7 +71,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}'],
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,mp3}'],
       },
       manifest: manifest,
     }),
